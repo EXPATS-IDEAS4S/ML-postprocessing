565b7025f4f898be3f0c69e02825915e076127c9
Claudia Acquistapace <cacquist@meteo.uni-koeln.de>
2026-06-04 18:34:19 +0000

    updated colors and convective classes now 5

--- utils/plotting/class_colors.py
+++ utils/plotting/class_colors.py
@@ -1,3 +1,5 @@
+"""
+old color combination for EUMETSAT talk 2025
 colors_per_class1_names = {
     '0': 'darkgray',
     '1': 'darkslategrey',
@@ -21,3 +23,36 @@ class_groups = {
         'Overcast': [5, 6, 7],
         'Broken Clouds': [0, 1, 8],
     }
+"""
+
+colors_per_class1_names = {
+    '0': 'green',
+    '1': 'orangered',
+    '2': 'darkslategrey',
+    '3': 'orange',
+    '4': 'deepskyblue',
+    '5': 'navajowhite',
+    '6': 'orchid',
+    '7': 'royalblue',
+    '8': 'crimson',
+    '9': 'goldenrod',
+    '10': 'magenta',
+    '11': 'dodgerblue',
+    '12': 'lightcoral',
+    '13': 'olive',
+    '14': 'crimson'
+}
+
+
+
+class_groups = {
+        'Convection': [1, 3, 5, 8, 6],
+        'Overcast': [4, 7],
+        'Broken Clouds': [0, 2],
+    }
+
+
+#class 1 > class 8
+#class 3 > class 1
+#class 5 > class 3
+#class 8 > class 3 
